7db0ed6a882e242f0817eb76103a4ec9b35c5e7b
Kaan Yilmaz <kaan-yilmaz@outlook.com.tr>
2018-07-26 21:51:39 +0300

    Update DualVNH5019MotorShield.h

--- DualVNH5019MotorShield.h
+++ DualVNH5019MotorShield.h
@@ -11,19 +11,19 @@ class DualVNH5019MotorShield
 
     DualVNH5019MotorShield(unsigned char INA1, unsigned char INB1, unsigned char EN1DIAG1, unsigned char CS1, 
                            unsigned char INA2, unsigned char INB2, unsigned char EN2DIAG2, unsigned char CS2,
-						   unsigned char INA3, unsigned char INB3, unsigned char EN3DIAG3, unsigned char CS3,
-						   unsigned char INA4, unsigned char INB4, unsigned char EN4DIAG4, unsigned char CS4); //User-defined pin selection. 
+			   unsigned char INA3, unsigned char INB3, unsigned char EN3DIAG3, unsigned char CS3,
+			   unsigned char INA4, unsigned char INB4, unsigned char EN4DIAG4, unsigned char CS4); //User-defined pin selection. 
                            
     DualVNH5019MotorShield(unsigned char INA1, unsigned char INB1, 
                            unsigned char EN1DIAG1, unsigned char CS1, 
                            unsigned char PWM1, unsigned char INA2,
                            unsigned char INB2, unsigned char EN2DIAG2, 
                            unsigned char CS2, unsigned char PWM2,
-						   unsigned char INA3, unsigned char INB3,
-						   unsigned char EN3DIAG3, unsigned char CS3,
-						   unsigned char PWM3, unsigned char INA4,
-						   unsigned char INB4, unsigned char EN4DIAG4,
-						   unsigned char CS4, unsigned char PWM4); // User-defined pin selection, with PWM pins
+			   unsigned char INA3, unsigned char INB3,
+			   unsigned char EN3DIAG3, unsigned char CS3,
+			   unsigned char PWM3, unsigned char INA4,
+			   unsigned char INB4, unsigned char EN4DIAG4,
+			   unsigned char CS4, unsigned char PWM4); // User-defined pin selection, with PWM pins
   
     // PUBLIC METHODS
     void init(); // Initialize TIMER 1, set the PWM to 20kHZ. 
